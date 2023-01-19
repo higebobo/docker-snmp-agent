6f0d01ed2bef574a2a08d81c0cc5226c65019d01
higebobo <higebobo@gmail.com>
2023-01-19 11:31:30 +0900

    modified

--- snmp-agent/entrypoint.sh
+++ snmp-agent/entrypoint.sh
@@ -5,11 +5,10 @@ set -e
 OPTIONS=$@
 MIBS_DIR=/usr/share/snmp/mibs
 
-##
-## Settings - Replace variables
-##
-
-# Modify snmp.conf
+# =============================================================
+# Settings - Replace variables
+# =============================================================
+## Modify snmp.conf
 sed -i".orig" "s/mibs :/mibs all/" /etc/snmp/snmp.conf
 
 # Modify snmpd.conf
@@ -20,28 +19,28 @@ sed -i -e "s/agentAddress  udp:127.0.0.1:161/agentAddress  udp:161/" \
     -i -e "s/rocommunity public  default    -V systemonly/rocommunity public default/" \
     -i -e "s/rocommunity6 public  default   -V systemonly/#rocommunity6 public  default   -V systemonly/" /etc/snmp/snmpd.conf
 
-# Replace sysContact if environment variable `ADMIN` defined
+## Replace sysContact if environment variable `ADMIN` defined
 if [ -n "${ADMIN}" ]; then
   sed -i -e "s/sysContact     Me <me@example.org>/sysContact     ${ADMIN}/" /etc/snmp/snmpd.conf
 fi
 
 
-##
-## Download mibs
-##
+# =============================================================
+# Download mibs
+# =============================================================
 download-mibs
 
-# Additional MIBs
+## Additional MIBs
 # wget http://www.iana.org/assignments/ianaippmmetricsregistry-mib/ianaippmmetricsregistry-mib -O ${MIBS_DIR}/iana/IANA-IPPM-METRICS-REGISTRY-MIB
 wget http://pastebin.com/raw/p3QyuXzZ -O ${MIBS_DIR}/ietf/SNMPv2-PDU
 wget http://pastebin.com/raw/gG7j8nyk -O ${MIBS_DIR}/ietf/IPATM-IPMC-MIB
 
-# Private MIBs
+## Private MIBs
 
 
-##
-## Run service
-##
+# =============================================================
+# Run service
+# =============================================================
 service snmpd start && /bin/bash
 
 
